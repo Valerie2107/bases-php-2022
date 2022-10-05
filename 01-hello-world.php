d1160f619db03170052293451542e1f1d9ed4e8e
valerie2107 <kinkamyl@hotmail.com>
2022-10-05 11:49:04 +0200

    hello world

---
+++ 01-hello-world.php
@@ -0,0 +1,22 @@
+<?php
+//on peut mettre un commentaire sur une ligne
+
+// on peut lettre des balises PHP n'importe où dans une page HTML
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php //ouverture du PHP
+    echo "<h1>Hello World</h1>";   //echo veut dire "affiche" (comme sur git)
+    //fermeture du PHP  ?>
+</body>
+</html>
