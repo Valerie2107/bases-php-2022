374dbdcbb4468aebef2b71f6455bf5852a0724ef
valerie2107 <kinkamyl@hotmail.com>
2022-10-28 12:20:11 +0200

    calculatrce c'est pas facile

--- 14-exe/14-calcul-avec-if-val.php
+++ 14-exe/14-calcul-avec-if-val.php
@@ -2,6 +2,19 @@
 
 if (isset($_POST['piouiou'],$_POST['symbole'],$_POST['cuicui'])&&(is_numeric($_POST['pioupiou'],$_POST['cuicui']))){
 
+
+    $calcul1 = (float) $_POST['pioupiou'];
+    $calcul2 = (float) $_POST['cuicui'];
+    
+
+        if($_POST['symbole']===)
+
+
+
+
+
+
+
     
 }
 
