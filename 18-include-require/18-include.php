2771dda5339d9f5301d7ef008563f371c6916d33
valerie2107 <kinkamyl@hotmail.com>
2022-10-28 12:05:55 +0200

    plus d'infos sur include et required

--- 18-include-require/18-include.php
+++ 18-include-require/18-include.php
@@ -9,6 +9,14 @@
 <body>
     <h1>include</h1>
     <p>include() est une fonction qui va permettre d'importer n'importe quoi (en général un autre fichier PHP), autant de fois qu'on le souhaite, et sans erreur grave en cas d'échec</p>
+
+    <!--   https://www.pierre-giraud.com/php-mysql-apprendre-coder-cours/inclure-fichier-include-require/  -->
+
+    <p>La seule et unique différence entre les instructions include et require va se situer dans la réponse du PHP dans le cas ou le fichier ne peut pas être inclus pour une quelconque raison (fichier introuvable, indisponible, etc.).<br>
+
+Dans ce cas-là, si l’inclusion a été tentée avec include, le PHP renverra un simple avertissement et le reste du script s’exécutera quand même tandis que si la même chose se produit avec require, une erreur fatale sera retournée par PHP et l’exécution du script s’arrêtera immédiatement.</p>
+
+
     <?php
     include "18-config.php";
     // appel le footer
