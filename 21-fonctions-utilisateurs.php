4aaf946c31344ce617b2ba6bbd71f31fd9c6b9f2
valerie2107 <kinkamyl@hotmail.com>
2022-11-03 15:04:06 +0100

    fichier 21

---
+++ 21-fonctions-utilisateurs.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les fonctions utilisateurs</title>
+</head>
+<body>
+    <h1>Les fonctions utilisateurs</h1>
+    <p>En PHP, on peut créer une nouvelle fonction en utilisant le mot-clef "function" suivit d'un nom (utilisation du même nommage que les variables sans le $) et de parenthèses qui peuvent contenir des arguments/paramètres, et le contenu de la fonctions entre accolades { }.</p>
+
+        <pre><code>
+        
+        function creeDesTensionsDansLeGroupe(){   
+                    return date("Y-m-d H:i:s");   
+                }   
+
+        </code></pre>
+        
+        <?php
+                function creeDesTensionsDansLeGroupe(){   //fonction créée sans argument dans cet exemple
+                    
+                    return date("Y-m-d H:i:s");   //https://www.php.net/manual/fr/datetime.format.php 
+                }   
+            //ici on utilise le return - comme le plus souvent - car on ne désire pas forcément afficher le résultat mais on veut STOCKER la valeur pour éventuellement la stocker
+
+            echo creeDesTensionsDansLeGroupe();
+            ?>
+
+    
+                <h2>Les fonctions avec arguments</h2>
+                    <p>Les paramètres ou arguments sont obligatoires si déclarés et sans valeur par défaut.</p>
+
+
+                    <pre><code>
+                        function calculBasic($num1, $num2, $op = "+"){    <!--op signifie opération -->
+
+                        }
+                    </code></pre>
+
+                    <?php
+                     function calculBasic($num1, $num2, $op = "+"){
+
+                    }
+
+
+                    ?>
+
+
+
+
+        
+
+            
+</body>
+</html>
