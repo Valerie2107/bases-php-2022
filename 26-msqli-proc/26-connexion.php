01c7e8cb586226c0572b11b65f9d6542d8edb72b
valerie2107 <kinkamyl@hotmail.com>
2022-11-18 11:09:23 +0100

    plus

---
+++ 26-msqli-proc/26-connexion.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion avec mysqli</title>
+</head>
+<body>
+    <h1>Connexion avec mysqli</h1>
+        <h2>méthode procédurale</h2>
+            <h3>mysqli_connect</h3>
+                <p>voir également sur <a href="https://www.php.net/manual/fr/mysqli.construct.php" target="_blank">php.net</a></p>
+
+                <?php
+                    $mysqliConnect = mysqli_connect("localhost","root")
+
+                    // rien n'est complet, j'ai lâché le cours
+
+
+                ?>
+
+</body>
+</html>
---
+++ 26-msqli-proc/readme.md
@@ -0,0 +1,9 @@
+# le choix de mysqli
+
+l'orienté objet (OO) est en réalité une architecture
+un objet contient en réalité en lui du procédural
+l'objet que je crée est comme une brique. et on doit après pouvoir construire un mur avec toutes ces briques.
+
+Nous faisons le choix de mysqli procédural pour nos connexions.
+Nous passerons à l'OO plus tard et en utilisant de préférene PDO:
+www.php.net/manual/fr/mysqlinfo.api.choosing.php
